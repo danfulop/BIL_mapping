b53d726e29a86fa1593b3330eec9b9601ae4d1b5
Daniel Fulop <dan.fulop.s@gmail.com>
2014-11-26 20:10:30 -0800

    fixed the prediction loops by referencing the correct data frames, not comp.rn.

--- BIL_phenos_LMM.R
+++ BIL_phenos_LMM.R
@@ -179,7 +179,7 @@ circ.pred <- vector("list", length=5)
 registerDoParallel(cores=4) # register parallel backend
 circ.pred <- foreach(i=1:5, .options.multicore=mcoptions) %dopar% { # run loop
   y = i + 4
-  circ.pred[i] <- predResp(x=comp.rn, y=y, randform="(1 | plant / leaflet.type)" )
+  circ.pred[i] <- predResp(x=circ, y=y, randform="(1 | plant / leaflet.type)" )
 }
 names(circ.pred) <- names(circ)[5:9] # name the pred elements by their original trait names
 save(circ.pred, file="circ.pred.Rdata") # Save prediction data.frames' list
@@ -189,7 +189,7 @@ sym.pred <- vector("list", length=9)
 registerDoParallel(cores=4) # register parallel backend
 sym.pred <- foreach(i=1:9, .options.multicore=mcoptions) %dopar% { # run loop
   y = i + 5
-  sym.pred[i] <- predResp(x=comp.rn, y=y, randform="(1 | plant / leaflet.type)" )
+  sym.pred[i] <- predResp(x=sym, y=y, randform="(1 | plant / leaflet.type)" )
 }
 names(sym.pred) <- names(sym)[6:14] # name the pred elements by their original trait names
 save(sym.pred, file="sym.pred.Rdata") # Save prediction data.frames' list
@@ -199,7 +199,7 @@ asym.pred <- vector("list", length=7)
 registerDoParallel(cores=4) # register parallel backend
 asym.pred <- foreach(i=1:7, .options.multicore=mcoptions) %dopar% { # run loop
   y = i + 5
-  asym.pred[i] <- predResp(x=comp.rn, y=y, randform="(1 | plant)" )
+  asym.pred[i] <- predResp(x=asym, y=y, randform="(1 | plant)" )
 }
 names(asym.pred) <- names(asym)[6:12] # name the pred elements by their original trait names
 save(asym.pred, file="asym.pred.Rdata") # Save prediction data.frames' list
