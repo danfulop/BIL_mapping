6e22e208a125f568c477fc195c5b2dd77255ced7
Daniel Fulop <dan.fulop.s@gmail.com>
2014-12-31 13:01:55 -0800

    Commented out genetic distance conversion code for bin.stats, and removed code for making "doubled" bin.stats

--- QTL_plots.R
+++ QTL_plots.R
@@ -4,48 +4,47 @@ library(ggplot2)
 library(circlize)
 library(scam)
 
-load(file="/Users/Dani/UCD/BILs/pen12t.Rdata")
-
-# Use shape constrained monotonically increasing general additive models to convert physical to genetic distance, using scam package
-
-fits <- vector('list', 12) # list for storing the scam model fits
-for (i in 1:length(levels(pen12t$chromosome))) {
-  chrom <- levels(pen12t$chromosome)[i]
-  fit.name <- paste0(chrom,"fit")
-  scamfit <- scam(formula=PEN12_Pos ~ 0 + s(position_bp, bs="mpi", k=30), data=pen12t[pen12t$chromosome==chrom,]) # use low k so that small pericentromeric bins show up more
-  fits[[i]] <- scamfit
-}
-save(fits, file="/Users/Dani/UCD/BILs/scam_fits.Rdata") # save scam fits' list object
+# load(file="/Users/Dani/UCD/BILs/pen12t.Rdata")
+# 
+# # Use shape constrained monotonically increasing general additive models to convert physical to genetic distance, using scam package
+# 
+# fits <- vector('list', 12) # list for storing the scam model fits
+# for (i in 1:length(levels(pen12t$chromosome))) {
+#   chrom <- levels(pen12t$chromosome)[i]
+#   fit.name <- paste0(chrom,"fit")
+#   scamfit <- scam(formula=PEN12_Pos ~ 0 + s(position_bp, bs="mpi", k=30), data=pen12t[pen12t$chromosome==chrom,]) # use low k so that small pericentromeric bins show up more
+#   fits[[i]] <- scamfit
+# }
+# save(fits, file="/Users/Dani/UCD/BILs/scam_fits.Rdata") # save scam fits' list object
 load("/Users/Dani/UCD/BILs/scam_fits.Rdata")
 
-load("/Users/Dani/UCD/BILs/leaf_traits/bin.stats.Rdata") # load bin information
-bin.stats$chr <- as.factor(substr(bin.stats$chr,7,10)) # Trim "SL2.40" from chromosome names
-num.trim.fx <- colwise(function(x) as.numeric(str_trim(x) ) ) # make a column-wise function to trim factor columns of white space and convert them to numeric. Coercion to char isn't needed since trimming does that already
-bin.stats[3:5] <- num.trim.fx(bin.stats[3:5] )
+# load("/Users/Dani/UCD/BILs/leaf_traits/bin.stats.Rdata") # load bin information
+# bin.stats$chr <- as.factor(substr(bin.stats$chr,7,10)) # Trim "SL2.40" from chromosome names
+# num.trim.fx <- colwise(function(x) as.numeric(str_trim(x) ) ) # make a column-wise function to trim factor columns of white space and convert them to numeric. Coercion to char isn't needed since trimming does that already
+# bin.stats[3:5] <- num.trim.fx(bin.stats[3:5] )
 
-# predict genetic distance on bin.stats' physical distance coordinates
-
-chrom <- levels(bin.stats$chr)
-bin.predict <- vector('list', 12) # list of tables for storing the scam model genetic distance predictions
-for (h in 1:length(chrom) ) {
-  chrom.tab <- bin.stats[bin.stats$chr==chrom[h], ]
-  # predict genetic distance 3 times with: bin.mid, bin.start, and bin.end
-  bin.mid <- data.frame(bin.mid=chrom.tab$bin.mid)
-  newdata <- data.frame(position_bp=bin.mid$bin.mid)
-  bin.mid$gen.bin.mid <- predict(fits[[h]], newdata=newdata, type="response", newdata.guaranteed=TRUE)
-  bin.start <- data.frame(bin.start=chrom.tab$bin.start)
-  newdata <- data.frame(position_bp=bin.start$bin.start)
-  bin.start$gen.bin.start <- predict(fits[[h]], newdata=newdata, type="response", newdata.guaranteed=TRUE)
-  bin.end <- data.frame(bin.end=chrom.tab$bin.end)
-  newdata <- data.frame(position_bp=bin.end$bin.end)
-  bin.end$gen.bin.end <- predict(fits[[h]], newdata=newdata, type="response", newdata.guaranteed=TRUE)
-  bin.predict[[h]] <- cbind(bin.mid, bin.start, bin.end) # cbind 3 predictions
-  zero.cM <- colwise(function(x, chr.start) x - (chr.start * chr.start/x ) ) # function to proportionally zero the predicted gen. dists.
-  bin.predict[[h]][, c(2,4,6)] <- zero.cM(bin.predict[[h]][, c(2,4,6)], chr.start = bin.start$gen.bin.start[1]) # zero the cM predictions
-}
-bin.predict <- do.call(rbind, bin.predict) # rbind bin.predict's 12 elements
-gen.bin.stats <- cbind(bin.stats[, 1:2], bin.predict) # cbind bin.stats' 1st 2 cols (i.e. bin & chr) & bin.predict
-save(gen.bin.stats, file="/Users/Dani/UCD/BILs/leaf_traits/gen.bin.stats.Rdata")
+# # predict genetic distance on bin.stats' physical distance coordinates
+# chrom <- levels(bin.stats$chr)
+# bin.predict <- vector('list', 12) # list of tables for storing the scam model genetic distance predictions
+# for (h in 1:length(chrom) ) {
+#   chrom.tab <- bin.stats[bin.stats$chr==chrom[h], ]
+#   # predict genetic distance 3 times with: bin.mid, bin.start, and bin.end
+#   bin.mid <- data.frame(bin.mid=chrom.tab$bin.mid)
+#   newdata <- data.frame(position_bp=bin.mid$bin.mid)
+#   bin.mid$gen.bin.mid <- predict(fits[[h]], newdata=newdata, type="response", newdata.guaranteed=TRUE)
+#   bin.start <- data.frame(bin.start=chrom.tab$bin.start)
+#   newdata <- data.frame(position_bp=bin.start$bin.start)
+#   bin.start$gen.bin.start <- predict(fits[[h]], newdata=newdata, type="response", newdata.guaranteed=TRUE)
+#   bin.end <- data.frame(bin.end=chrom.tab$bin.end)
+#   newdata <- data.frame(position_bp=bin.end$bin.end)
+#   bin.end$gen.bin.end <- predict(fits[[h]], newdata=newdata, type="response", newdata.guaranteed=TRUE)
+#   bin.predict[[h]] <- cbind(bin.mid, bin.start, bin.end) # cbind 3 predictions
+#   zero.cM <- colwise(function(x, chr.start) x - (chr.start * chr.start/x ) ) # function to proportionally zero the predicted gen. dists.
+#   bin.predict[[h]][, c(2,4,6)] <- zero.cM(bin.predict[[h]][, c(2,4,6)], chr.start = bin.start$gen.bin.start[1]) # zero the cM predictions
+# }
+# bin.predict <- do.call(rbind, bin.predict) # rbind bin.predict's 12 elements
+# gen.bin.stats <- cbind(bin.stats[, 1:2], bin.predict) # cbind bin.stats' 1st 2 cols (i.e. bin & chr) & bin.predict
+# save(gen.bin.stats, file="/Users/Dani/UCD/BILs/leaf_traits/gen.bin.stats.Rdata")
 load("/Users/Dani/UCD/BILs/leaf_traits/gen.bin.stats.Rdata")
 
 # Additive plots
@@ -172,26 +171,9 @@ plot.map(FT.map, bin.stats, dat.name="FT")
 
 # Epistatic plots
 #-----------
-# Modify bin.stats to have doubled information
 load("/Users/Dani/UCD/BILs/leaf_traits/bin.stats.Rdata") # load bin information
 bin.stats$chr <- as.factor(substr(bin.stats$chr,7,10)) # Trim "SL2.40" from chromosome names
 num.trim.fx <- colwise(function(x) as.numeric(str_trim(x) ) ) # make a column-wise function to trim factor columns of white space and convert them to numeric. Coercion to char isn't needed since trimming does that already
-bin.stats[3:5] <- num.trim.fx(bin.stats[3:5] )
-# head(bin.stats)
-# summary(bin.stats)
-bin.stats$bin2 <- bin.stats$bin
-bin.stats$chr2 <- bin.stats$chr
-bin.stats$bin.mid2 <- bin.stats$bin.mid
-bin.stats$bin.start2 <- bin.stats$bin.start
-bin.stats$bin.end2 <- bin.stats$bin.end
-names(bin.stats)[1:5] <- paste0(names(bin.stats)[1:5], "1")
-head(bin.stats)
-summary(bin.stats)
-doubled.bin.stats <- bin.stats
-
-load("/Users/Dani/UCD/BILs/leaf_traits/bin.stats.Rdata") # load bin information
-bin.stats$chr <- as.factor(substr(bin.stats$chr,7,10)) # Trim "SL2.40" from chromosome names
-num.trim.fx <- colwise(function(x) as.numeric(str_trim(x) ) ) 
 bin.stats[3:5] <- num.trim.fx(bin.stats[3:5] )
 # create a data.frame with chromosome start and end info. to initialize the circos plot
 circ.init <- lapply(1:nlevels(bin.stats$chr), function(i) {
